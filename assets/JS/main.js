e3a6f79eb6e832a17b77798cfcb84299401148f7
Yuutsuy <kiraidase@gmail.com>
2022-04-29 15:33:07 -0300

    Falha ao acessar a API, mesmo com internet do celular, a API não me retorna e além disso, me retorna o status de 200 com erro

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -2,17 +2,23 @@ var origem = document.getElementById('origem');
 var destino = document.getElementById('destino');
 
 if(origem.value !== '' && destino.value !== ''){
-    if(origem.value === xhr.origem && destino.value === xhr.destino){
-        var xhr = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
 
-        xhr.open('GET', `https://barth.com.br/ApiChatCliqx/chat/verificarMensagem.php?origem=${origem.value}&destino=${destino.value}`);
-        xhr.send(null);
-        xhr.onreadystatechange = function() {
-            if(xhr.readyState === 4){
-                if(xhr.status === 200){
-                    alert('acessou')
-                    
-                }
+    xhr.open('GET', `https://barth.com.br/ApiChatCliqx/chat/verificarMensagem.php?origem=${origem.value}&destino=${destino.value}`);
+    xhr.send(null);
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4){
+            console.log(xhr);
+        }
+    }
+}
+var teste = function(){
+    xhr.open('POST', 'https://barth.com.br/ApiChatCliqx/chat/inserirMensagem.php');
+    xhr.send(msgDigitada.value);
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                
             }
         }
     }
