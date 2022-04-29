1c58b356535060c2f6314bda90376daef02c44d6
Yuutsuy <kiraidase@gmail.com>
2022-04-29 14:07:28 -0300

    criado condição para teste no js

---
+++ assets/JS/main.js
@@ -0,0 +1,19 @@
+var origem = document.getElementById('origem');
+var destino = document.getElementById('destino');
+
+if(origem.value !== '' && destino.value !== ''){
+    if(origem.value === xhr.origem && destino.value === xhr.destino)
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', `https://barth.com.br/ApiChatCliqx/chat/verificarMensagem.php?origem=${origem.value}&destino=${destino.value}`);
+    xhr.send(null);
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                alert('acessou')
+                console.log(xhr.origem);
+                console.log(xhr.destino);
+            }
+        }
+    }
+}
