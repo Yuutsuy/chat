4f9c592f39361a1e5c653f3310ff2f251dc4f80a
Yuutsuy <kiraidase@gmail.com>
2022-04-29 17:26:51 -0300

    ainda com problemas para acessar a API

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,5 +1,6 @@
 var origem = document.getElementById('origem');
 var destino = document.getElementById('destino');
+var mensagem = document.getElementById('msgDigitada');
 
 if(origem.value !== '' && destino.value !== ''){
     var xhr = new XMLHttpRequest();
@@ -8,17 +9,24 @@ if(origem.value !== '' && destino.value !== ''){
     xhr.send(null);
     xhr.onreadystatechange = function() {
         if(xhr.readyState === 4){
-            console.log(xhr);
+            
         }
     }
 }
+
+var obj = {
+    origem : origem.value,
+    destino : destino.value,
+    mensagem : mensagem.value
+};
+
 var teste = function(){
     xhr.open('POST', 'https://barth.com.br/ApiChatCliqx/chat/inserirMensagem.php');
-    xhr.send(msgDigitada.value);
+    xhr.send(obj);
     xhr.onreadystatechange = function() {
         if(xhr.readyState === 4){
             if(xhr.status === 200){
-                
+               
             }
         }
     }
