3b5fd356080b13895b3823a24054aaa895ff528d
Yuutsuy <kiraidase@gmail.com>
2022-05-02 11:27:55 -0300

    Finalmente API acessada(graças ao professor)

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -2,32 +2,36 @@ var origem = document.getElementById('origem');
 var destino = document.getElementById('destino');
 var mensagem = document.getElementById('msgDigitada');
 
-if(origem.value !== '' && destino.value !== ''){
-    var xhr = new XMLHttpRequest();
-
-    xhr.open('GET', `https://barth.com.br/ApiChatCliqx/chat/verificarMensagem.php?origem=${origem.value}&destino=${destino.value}`);
-    xhr.send(null);
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState === 4){
-            
-        }
-    }
-}
-
 var obj = {
-    origem : origem.value,
-    destino : destino.value,
-    mensagem : mensagem.value
+    "origem" : origem.value,
+    "destino" : destino.value,
+    "mensagem" : mensagem.value
 };
 
-var teste = function(){
-    xhr.open('POST', 'https://barth.com.br/ApiChatCliqx/chat/inserirMensagem.php');
-    xhr.send(obj);
+var xhr = new XMLHttpRequest(); // recebe instância
+
+xhr.open('GET', `https://barth.com.br/ApiChatCliqx/chat/verificarMensagem.php?origem=${obj.origem}&lt;nome_origem&gt;&amp;destino=${obj.destino}&lt;nome_destino&gt;`);
+xhr.send(null);
+xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4){
+        if(xhr.status === 200){
+            console.log(xhr.obj);
+        }else{
+            console.log("erro no status do get");
+        };
+    };
+};
+
+var enviar = function() {
+    var xhr = new XMLHttpRequest(); // recebe instância
+
+    xhr.open('POST', `https://barth.com.br/ApiChatCliqx/chat/inserirMensagem.php`);
+    xhr.send(JSON.parse(obj));
     xhr.onreadystatechange = function() {
         if(xhr.readyState === 4){
-            if(xhr.status === 200){
-               
-            }
-        }
-    }
-}
+            if(xhr.status === 201){
+                console.log(xhr.responseText);
+            };
+        };
+    };
+};
