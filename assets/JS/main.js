555ad12675e2b2d4b1591e8c7c19bd4293f444a9
Yuutsuy <kiraidase@gmail.com>
2022-05-03 11:54:02 -0300

    Completado chat de conversa(em andamento tela de login)

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,2 +1,11 @@
 var elEmail = document.getElementById('email');
 var elSenha = document.getElementById('senha');
+
+var objLogin = {
+    email : elEmail.value,
+    senha : elSenha.value
+}
+
+var logar = function(){
+     var xhr = XMLHttpRequest
+}
