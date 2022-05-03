b97501f804ac486c38b819c3ef536365ec3ce477
Yuutsuy <kiraidase@gmail.com>
2022-05-03 16:11:19 -0300

    acessado api com sucesso e tratado a chave login, mas o console deu problema dnv... T^T

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -2,18 +2,23 @@ var elEmail = document.getElementById('email');
 var elSenha = document.getElementById('senha');
 
 var logar = function(){
-    
-    var xhr = new XMLHttpRequest();
+    setInterval(function(){
+        var xhr = new XMLHttpRequest();
 
-    xhr.open(
-        'GET', 
-        `https://barth.com.br/ApiChatCliqx/chat/verificarLogin.php?email=${elEmail.value}&senha=${elSenha.value}`);
-    xhr.send(null);
-    xhr.onreadystatechange = function(){
-        if(xhr.readyState === 4){
-            if(xhr.status === 200){
-                
-            }
-        }
-    }
-}
+        xhr.open(
+            'GET', 
+            `https://barth.com.br/ApiChatCliqx/chat/verificarLogin.php?email=${elEmail.value}&senha=${elSenha.value}`);
+        xhr.send(null);
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === 4){
+                if(xhr.status === 200){
+                    var res = JSON.parse(xhr.responseText);
+                    if(res.login === true){
+                        console.log('elaia')
+                    };
+                };
+            };
+        }; 
+    }, 1000);
+    
+};
